d4add73c9d9ee005b0a3f353b4abab5a3ecfd6b9
ColonParenColon <poisonousbagel343@gmail.com>
2026-04-28 00:08:45 -0500

    Initial Mod

---
+++ Enwrinkled Kontaalen Armada.manifest.lua
@@ -0,0 +1,7 @@
+﻿return {
+    modName = "Enwrinkled Kontaalen Armada",
+    author = ":):",
+    id = "220BrainKon",
+    version = "1.0.0",
+    description = "The Kontaalen armada, if they stopped caring about 'balance' and started 'locking in' and trying to win.",
+}
